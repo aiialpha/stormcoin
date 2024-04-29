1f639c12115172a43cf85c763c4b45e742f26c95
aiialpha <aii07038713563@gmail.com>
2024-04-29 17:17:47 +0100

    save

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import logo from '../assets/tapps.png'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div className="nav-tabs">
+            <a href="">
+                <img src={logo} alt="logo" />
+                <span>
+                    Hello
+                </span>
+            </a>
+        </div>
+        <div className="nav-tabs">
+            <a href="">
+                <img src={logo} alt="logo" />
+                <span>
+                    Hello
+                </span>
+            </a>
+        </div>
+        <div className="nav-tabs">
+            <a href="">
+                <img src={logo} alt="logo" />
+                <span>
+                    Hello
+                </span>
+            </a>
+        </div>
+        <div className="nav-tabs">
+            <a href="">
+                <img src={logo} alt="logo" />
+                <span>
+                    Hello
+                </span>
+            </a>
+        </div>
+    </nav>
+  )
+}
